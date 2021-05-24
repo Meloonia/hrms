db1fbff01b6567ee1bd85309d7debacda00b6888
ilker çelimli <meloonia52@gmail.com>
2021-05-24 12:31:15 +0300

    boş alan kontrolü eklendi.
    boş alan kontrolü eklendi.

--- src/main/java/io/kodlama/Business/Concretes/JobSeekerManager.java
+++ src/main/java/io/kodlama/Business/Concretes/JobSeekerManager.java
@@ -7,7 +7,7 @@ import io.kodlama.Core.utilities.results.SuccessResult;
 import io.kodlama.Core.utilities.results.UnsuccessfulResult;
 import io.kodlama.DataAccess.Abstracts.JobSeekerManagerDao;
 import io.kodlama.DataAccess.Abstracts.UserManagerDao;
-import io.kodlama.Entites.Concretes.EmailValidationEntity;
+
 import io.kodlama.Entites.Concretes.JobSeekerEntity;
 import io.kodlama.Entites.Concretes.UserEntity;
 import io.kodlama.adapters.Inmemory.Abstracts.EmailValidation;
@@ -40,32 +40,35 @@ public class JobSeekerManager implements JobSeekerService {
     public Result insert(UserEntity user, JobSeekerEntity jobSeeker) {
 
         try {
-            if(mernis.TCNoDogrula(jobSeeker.getJobSeekerNationalId() ,
-                    jobSeeker.getJobSeekerName()
-                    , jobSeeker.getJobSeekerSurname(),jobSeeker.getBirtday())) {
-                if (
-                        jobSeekerService.findAll().stream().anyMatch(j -> j.getJobSeekerNationalId() ==
-                                jobSeeker.getJobSeekerNationalId())) {
-                    if(userManagerDao.findAll().stream().anyMatch(u -> u.getUserEmail().equals(user.getRePassword()))){
+            if(user.getUserEmail().length() == 0 && user.getRePassword().length() == 0 &&
+           !user.getRePassword().isEmpty() && jobSeeker.getJobSeekerName().length() == 0
+           && jobSeeker.getJobSeekerSurname().length() == 0
+           && String.valueOf(jobSeeker.getJobSeekerNationalId()).length() == 0
+           && String.valueOf(jobSeeker.getBirtday()).length() == 0
+           && jobSeeker.getJobSeekerPhone().length() == 0
+           && jobSeeker.getJobSeekerAdress().length() == 0) {
+                if (mernis.TCNoDogrula(jobSeeker.getJobSeekerNationalId(),
+                        jobSeeker.getJobSeekerName()
+                        , jobSeeker.getJobSeekerSurname(), jobSeeker.getBirtday())) {
+                    if (
+                            jobSeekerService.findAll().stream().anyMatch(j -> j.getJobSeekerNationalId() ==
+                                    jobSeeker.getJobSeekerNationalId())) {
+                        if (userManagerDao.findAll().stream().anyMatch(u -> u.getUserEmail().equals(user.getUserEmail()))) {
 
-                        emailValidation.sendMail();
+                            emailValidation.sendMail();
 
-                       if (emailValidation.EmailDogrula(emailValidation.EmailDogrula(), user)) {
-                           userManager.insertUser(user);
-                           jobSeekerService.save(jobSeeker);
-                           return new SuccessResult(true,"Kullanıcı başarı ile kaydedildi.");
-                       }
-                        else return new UnsuccessfulResult(false,"Email Doğrulanmadı");
-                    }
-                else return new UnsuccessfulResult(false , "Email Kayıtlı");
+                            if (emailValidation.EmailDogrula(emailValidation.EmailDogrula(), user)) {
+                                userManager.insertUser(user);
+                                jobSeekerService.save(jobSeeker);
+                                return new SuccessResult(true, "Kullanıcı başarı ile kaydedildi.");
+                            } else return new UnsuccessfulResult(false, "Email Doğrulanmadı");
+                        } else return new UnsuccessfulResult(false, "Email Kayıtlı");
 
 
-            }
-                        else return new UnsuccessfulResult(false, "kullanıcı zaten kayıtlı.");
-                }
-            else return new UnsuccessfulResult(false,"Mernis doğrulanamıyor");
+                    } else return new UnsuccessfulResult(false, "kullanıcı zaten kayıtlı.");
+                } else return new UnsuccessfulResult(false, "Mernis doğrulanamıyor");
 
-
+            } else return new UnsuccessfulResult(false,"Alanlar boş bırakılamaz.");
         }
         catch (Exception e) {
 
