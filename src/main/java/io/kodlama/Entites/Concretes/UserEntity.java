11dd3938e6796335488acbeaaa925c91cc1c9e79
ilker çelimli <meloonia52@gmail.com>
2021-05-24 16:14:09 +0300

    Update
    JobSeekerManager refactor öncesi güncelleme

--- src/main/java/io/kodlama/Entites/Concretes/UserEntity.java
+++ src/main/java/io/kodlama/Entites/Concretes/UserEntity.java
@@ -1,5 +1,6 @@
 package io.kodlama.Entites.Concretes;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,20 +14,22 @@ import javax.persistence.*;
 @Table(name = "users")
 public class UserEntity {
 
+
     @Column
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long UserId;
 
+    @NotNull
     @Column(name = "user_email")
     private String UserEmail;
-
+    @NotNull
     @Column(name = "user_password")
     private String UserPassword;
-
+    @NotNull
     @Column(name = "user_repassword")
     private String rePassword;
-
+    @NotNull
     @Column(name = "user_Email_validation")
     private boolean emailValidation;
 
