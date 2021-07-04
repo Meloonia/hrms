722532faaa85ad642004377697ee6066b8c59b55
ilker <meloonia52@gmail.com>
2021-07-04 19:54:52 +0300

    update

---
+++ target/generated-sources/annotations/io/kodlama/Entites/Mapper/BusinessSectorToEmployerDtoConverterImpl.java
@@ -0,0 +1,47 @@
+package io.kodlama.Entites.Mapper;
+
+import io.kodlama.Entites.Concretes.JobAdverstisementEntity;
+import io.kodlama.Entites.dto.BusinessSectorToEmployerDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-07-04T19:54:36+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16.0.1 (Oracle Corporation)"
+)
+@Component
+public class BusinessSectorToEmployerDtoConverterImpl implements BusinessSectorToEmployerDtoConverter {
+
+    @Override
+    public List<BusinessSectorToEmployerDto> businessSectorToEmployerDtoConverter(List<JobAdverstisementEntity> businessSectorToEmployerDto) {
+        if ( businessSectorToEmployerDto == null ) {
+            return null;
+        }
+
+        List<BusinessSectorToEmployerDto> list = new ArrayList<BusinessSectorToEmployerDto>( businessSectorToEmployerDto.size() );
+        for ( JobAdverstisementEntity jobAdverstisementEntity : businessSectorToEmployerDto ) {
+            list.add( jobAdverstisementEntityToBusinessSectorToEmployerDto( jobAdverstisementEntity ) );
+        }
+
+        return list;
+    }
+
+    protected BusinessSectorToEmployerDto jobAdverstisementEntityToBusinessSectorToEmployerDto(JobAdverstisementEntity jobAdverstisementEntity) {
+        if ( jobAdverstisementEntity == null ) {
+            return null;
+        }
+
+        BusinessSectorToEmployerDto businessSectorToEmployerDto = new BusinessSectorToEmployerDto();
+
+        businessSectorToEmployerDto.setMaxSalary( jobAdverstisementEntity.getMaxSalary() );
+        businessSectorToEmployerDto.setOpenPosition( jobAdverstisementEntity.getOpenPosition() );
+        businessSectorToEmployerDto.setRelaseDate( jobAdverstisementEntity.getRelaseDate() );
+        businessSectorToEmployerDto.setActiveDate( jobAdverstisementEntity.getActiveDate() );
+        businessSectorToEmployerDto.setActive( jobAdverstisementEntity.isActive() );
+
+        return businessSectorToEmployerDto;
+    }
+}
